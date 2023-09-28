614c2d6444940c1dadd887807ff31acb7e5366dd
Othavio Luis <76593685+OthavioLuis@users.noreply.github.com>
2023-09-28 00:25:58 -0300

    Add files via upload
    Minha primeira taboada com a linguagem C#, aula de algoritmo, Etec.

---
+++ taboada_c#.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taboada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("digite valor da taboada:");
+            b = int.Parse(Console.ReadLine());
+
+            for (a = 1; a <= 10; a++)
+            {
+                c = b * a;
+                Console.WriteLine(+b + " X " + a + " = " + c);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
