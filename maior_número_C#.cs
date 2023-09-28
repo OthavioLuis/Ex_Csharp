d11d611174d3b04154216caceb6e4c779d99bcdb
Othavio Luis <76593685+OthavioLuis@users.noreply.github.com>
2023-09-28 00:39:01 -0300

    Add files via upload
    Sistema de contar o maior número digitado. Aula Algoritmo em C#

---
+++ maior_número_C#.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maior_nmr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            
+            Console.Write("Digite o 1° número: ");
+
+            b = int.Parse(Console.ReadLine());
+            for (c = 1; c < 15; c++)
+
+            {
+                Console.Write("Digite o {0}° número: ", c + 1);
+                a = int.Parse(Console.ReadLine());
+
+                if (a > b)
+                {
+                    b = a;
+                }
+            }
+            Console.WriteLine("\nO maior número digitado foi: {0}", b);
+
+            Console.ReadKey();
+        }
+    }
+}
