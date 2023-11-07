c7c5d7428f9a5f917a78af182d6026f908737cb2
Othavio Luis <76593685+OthavioLuis@users.noreply.github.com>
2023-11-07 13:15:35 -0300

    Teste de vetores
    Programa que irá guardar nomes, teste de 4 nomes com vetor.

---
+++ nomes_vetor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[4];
+            for (int x = 0; x < 4; x++)
+            {
+                Console.Write("Digite um nome: ");
+                nomes[x] = Console.ReadLine();
+            }
+            Console.WriteLine("Nomes guardados no banco de dados.");
+            Console.WriteLine("Pressione 'Enter' para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
