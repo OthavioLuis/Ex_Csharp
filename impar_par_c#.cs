8e948d1c1bc932a4177b4239a0b5b5ea010e549b
Othavio Luis <76593685+OthavioLuis@users.noreply.github.com>
2024-02-02 23:36:31 -0300

    Add files via upload

---
+++ impar_par_c#.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impar_ou_par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        while (true) { 
+            int num;
+
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Digite um número para descobrir se é IMPAR ou PAR: ");
+            Console.WriteLine("-----------------------------------------------");
+            num = int.Parse(Console.ReadLine());
+
+            if (RestoPorDois(num) == 0)
+                Console.WriteLine("\n\nO número é par. \n");
+            else
+                Console.WriteLine("\n\nO número é impar. \n");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static int RestoPorDois(int a)
+        {
+            return a % 2;
+        }
+    }
+}
